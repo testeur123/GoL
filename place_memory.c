c2018d51639b956becd9a238a5af0f77bb284aee
franck <alphacentorus@gmail.com>
2012-12-24 00:53:25 +0100

    blabla

---
+++ place_memory.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+	int a=312121151454685465456456446465;
+	
+
+
+	printf("taille du int :%ld \n",sizeof(a) );
+
+	return 0;
+}
